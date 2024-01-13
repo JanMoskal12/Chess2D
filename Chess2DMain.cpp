7fd1450dd91649f0fd9819cb0269ddbf683244c1
Smakulec <szymon.makulec@onet.pl>
2024-01-13 17:06:24 +0100

    pełny plik bo tablicach2d

--- Chess2DMain.cpp
+++ Chess2DMain.cpp
@@ -46,8 +46,8 @@ wxString wxbuildinfo(wxbuildinfoformat format){
 
 
 { // Declaration of board and images
-wxBitmapButton* board[8][8];
-wxBitmap images[2][13];
+wxBitmapButton* (board[8][8]);
+wxBitmap (images[2][13]);
 }
 
 
@@ -83,45 +83,46 @@ Chess2DDialog::Chess2DDialog(wxWindow* parent,wxWindowID id)
     { // ChessBoard Display
     images[1][0] = wxBitmap(wxImage("images/B.jpg"));
     images[0][0] = wxBitmap(wxImage("images/D.jpg"));
-    images[0][1] = wxBitmap(wxImage(_("images/Pieces/brB.png")));
-    images[3] = wxBitmap(wxImage(_("images/Pieces/brD.png")));
-    images[4] = wxBitmap(wxImage(_("images/Pieces/bnB.png")));
-    images[5] = wxBitmap(wxImage(_("images/Pieces/bnD.png")));
-    images[6] = wxBitmap(wxImage(_("images/Pieces/bbB.png")));
-    images[7] = wxBitmap(wxImage(_("images/Pieces/bbD.png")));
-    images[8] = wxBitmap(wxImage(_("images/Pieces/bqB.png")));
-    images[9] = wxBitmap(wxImage(_("images/Pieces/bqD.png")));
-    images[10] = wxBitmap(wxImage(_("images/Pieces/bkB.png")));
-    images[11] = wxBitmap(wxImage(_("images/Pieces/bkD.png")));
-    images[12] = wxBitmap(wxImage(_("images/Pieces/bpB.png")));
-    images[13] = wxBitmap(wxImage(_("images/Pieces/bpD.png")));
-    images[14] = wxBitmap(wxImage(_("images/Pieces/wrB.png")));
-    images[15] = wxBitmap(wxImage(_("images/Pieces/wrD.png")));
-    images[16] = wxBitmap(wxImage(_("images/Pieces/wnB.png")));
-    images[17] = wxBitmap(wxImage(_("images/Pieces/wnD.png")));
-    images[18] = wxBitmap(wxImage(_("images/Pieces/wbB.png")));
-    images[19] = wxBitmap(wxImage(_("images/Pieces/wbD.png")));
-    images[20] = wxBitmap(wxImage(_("images/Pieces/wqB.png")));
-    images[21] = wxBitmap(wxImage(_("images/Pieces/wqD.png")));
-    images[22] = wxBitmap(wxImage(_("images/Pieces/wkB.png")));
-    images[23] = wxBitmap(wxImage(_("images/Pieces/wkD.png")));
-    images[24] = wxBitmap(wxImage(_("images/Pieces/wpB.png")));
-    images[25] = wxBitmap(wxImage(_("images/Pieces/wpD.png")));
+    images[0][1] = wxBitmap(wxImage(_T("images/Pieces/bpB.png")));
+    images[0][2] = wxBitmap(wxImage(_T("images/Pieces/bpD.png")));
+    images[0][3] = wxBitmap(wxImage(_T("images/Pieces/brB.png")));
+    images[0][4] = wxBitmap(wxImage(_T("images/Pieces/brD.png")));
+    images[0][5] = wxBitmap(wxImage(_T("images/Pieces/bnB.png")));
+    images[0][6] = wxBitmap(wxImage(_T("images/Pieces/bnD.png")));
+    images[0][7] = wxBitmap(wxImage(_T("images/Pieces/bbB.png")));
+    images[0][8] = wxBitmap(wxImage(_T("images/Pieces/bbD.png")));
+    images[0][9] = wxBitmap(wxImage(_T("images/Pieces/bqB.png")));
+    images[0][10] = wxBitmap(wxImage(_T("images/Pieces/bqD.png")));
+    images[0][11] = wxBitmap(wxImage(_T("images/Pieces/bkB.png")));
+    images[0][12] = wxBitmap(wxImage(_T("images/Pieces/bkD.png")));
 
-    board[0] = BitmapButton1;
-    board[1] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bnD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[2] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bbB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[3] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bqD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[4] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bkB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[5] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bbD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[6] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bnB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[7] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/brD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    images[1][1] = wxBitmap(wxImage(_T("images/Pieces/wpB.png")));
+    images[1][2] = wxBitmap(wxImage(_T("images/Pieces/wpD.png")));
+    images[1][3] = wxBitmap(wxImage(_T("images/Pieces/wrB.png")));
+    images[1][4] = wxBitmap(wxImage(_T("images/Pieces/wrD.png")));
+    images[1][5] = wxBitmap(wxImage(_T("images/Pieces/wnB.png")));
+    images[1][6] = wxBitmap(wxImage(_T("images/Pieces/wnD.png")));
+    images[1][7] = wxBitmap(wxImage(_T("images/Pieces/wbB.png")));
+    images[1][8] = wxBitmap(wxImage(_T("images/Pieces/wbD.png")));
+    images[1][9] = wxBitmap(wxImage(_T("images/Pieces/wqB.png")));
+    images[1][10] = wxBitmap(wxImage(_T("images/Pieces/wqD.png")));
+    images[1][12] = wxBitmap(wxImage(_T("images/Pieces/wkD.png")));
+    images[1][11] = wxBitmap(wxImage(_T("images/Pieces/wkB.png")));
+
+    board[0][0] = BitmapButton1;
+    board[0][1] = new wxBitmapButton(this, wxNewId(), images[0][6], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[0][2] = new wxBitmapButton(this, wxNewId(), images[0][7], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[0][3] = new wxBitmapButton(this, wxNewId(), images[0][10], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[0][4] = new wxBitmapButton(this, wxNewId(), images[0][11], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[0][5] = new wxBitmapButton(this, wxNewId(), images[0][8], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[0][6] = new wxBitmapButton(this, wxNewId(), images[0][5], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[0][7] = new wxBitmapButton(this, wxNewId(), images[0][4], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
 
     for(int j = 0; j < 8; j++){
-            if((j)%2 == 0){
-                board[8+j] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bpD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            if(j%2 == 0){
+                board[1][j] = new wxBitmapButton(this, wxNewId(),images[0][2] , wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
             }else{
-                board[8+j] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/bpB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+                board[1][j] = new wxBitmapButton(this, wxNewId(),images[0][1] , wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
 
             }
     }
@@ -129,38 +130,38 @@ Chess2DDialog::Chess2DDialog(wxWindow* parent,wxWindowID id)
     for(int i = 2; i < 6; i++){
         for(int j = 0; j < 8; j++){
             if((i+j)%2 == 0){
-                board[8*i+j] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/B.jpg"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+                board[i][j] = new wxBitmapButton(this, wxNewId(), images[1][0], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
             }else{
-                board[8*i+j] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/D.jpg"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+                board[i][j] = new wxBitmapButton(this, wxNewId(), images[0][0], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
             }
         }
     }
 
     for(int j = 0; j < 8; j++){
         if((j)%2 != 0){
-            board[48+j] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wpD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            board[6][j] = new wxBitmapButton(this, wxNewId(), images[0][1], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
         }else{
-            board[48+j] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wpB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+            board[6][j] = new wxBitmapButton(this, wxNewId(), images[0][2], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
 
         }
     }
 
-    board[56] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wrD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[57] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wnB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[58] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wbD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[59] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wqB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[60] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wkD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[61] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wbB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[62] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wnD.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
-    board[63] = new wxBitmapButton(this, wxNewId(), wxBitmap(wxImage(_T("images/Pieces/wrB.png"))), wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][0] = new wxBitmapButton(this, wxNewId(), images[1][4], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][1] = new wxBitmapButton(this, wxNewId(), images[1][5], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][2] = new wxBitmapButton(this, wxNewId(), images[1][8], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][3] = new wxBitmapButton(this, wxNewId(), images[1][9], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][4] = new wxBitmapButton(this, wxNewId(), images[1][12], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][5] = new wxBitmapButton(this, wxNewId(), images[1][7], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][6] = new wxBitmapButton(this, wxNewId(), images[1][6], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
+    board[7][7] = new wxBitmapButton(this, wxNewId(), images[1][3], wxDefaultPosition, wxDefaultSize, wxBU_AUTODRAW, wxDefaultValidator);
 
     for(int i = 0;i < 8; i++){
         for(int j = 0; j < 8; j++){
         if(i + j == 0){
             j++;
         }
-        GridSizer1->Add(board[8*i+j], 1, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
-        Connect(board[8*i+j]->GetId(),wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&Chess2DDialog::OnBitmapButton1Click);
+        GridSizer1->Add(board[i][j], 1, wxALIGN_CENTER_HORIZONTAL|wxALIGN_CENTER_VERTICAL, 5);
+        Connect(board[i][j]->GetId(),wxEVT_COMMAND_BUTTON_CLICKED,(wxObjectEventFunction)&Chess2DDialog::OnBitmapButton1Click);
 
 
         }
